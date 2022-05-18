f66e10645f4dda5925c3c316d0d89251bd62ee01
Ashish <45767581+vattevaii@users.noreply.github.com>
2022-05-18 16:15:56 +0545

    Update server.py

--- server.py
+++ server.py
@@ -104,4 +104,4 @@ def add_predict2():
 # A method that runs the application server.
 if __name__ == "__main__":
     # Threaded option to enable multiple instances for multiple user access support
-    app.run(debug=False, threaded=True, port=7000)
+    app.run(debug=False, threaded=True)
